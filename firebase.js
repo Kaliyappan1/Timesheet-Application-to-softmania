b384f5f58d132d293dda9cb98880d2f288e131a5
kaliyappan <yappank17@gmail.com>
2024-07-30 14:37:00 +0530

    firebase deploy added

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0PpvyuT53G9O2neIA2Ihq2qFUvPs5oUs",
+  authDomain: "softmania-timesheet.firebaseapp.com",
+  projectId: "softmania-timesheet",
+  storageBucket: "softmania-timesheet.appspot.com",
+  messagingSenderId: "489018279790",
+  appId: "1:489018279790:web:048d4dc376ff8cb14f237f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
