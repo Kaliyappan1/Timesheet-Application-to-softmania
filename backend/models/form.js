28a1228957ed94ab1c8daed39885362be461d473
kaliyappan <yappank17@gmail.com>
2024-08-28 19:42:09 +0530

    form page add required option and attendance filed absent present click change text fileds

--- backend/models/form.js
+++ backend/models/form.js
@@ -18,17 +18,33 @@ const formSchema = new mongoose.Schema({
   },
   workHours: {
     type: Number,
-    required: true,
   },
   topics: {
-    type: String, // Change from enum to String
-    required: true,
+    type: String,
+  },
+  reason: {
+    type: String,
   },
   description: {
     type: String,
   },
 }, {
   timestamps: true,
+});
+
+// Custom validation to ensure conditional requirements are met
+formSchema.pre('validate', function (next) {
+  if (this.attendance === 'Absent' && (this.workHours || this.topics)) {
+    return next(new Error("Work Hours and Topics should not be provided when attendance is Absent."));
+  }
+
+  if (this.attendance === 'Late' || this.attendance === 'Absent') {
+    if (!this.reason) {
+      return next(new Error('Reason is required for Late or Absent attendance.'));
+    }
+  }
+
+  next();
 });
 
 const Form = mongoose.model('Form', formSchema);
