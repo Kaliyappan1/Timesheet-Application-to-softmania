87fa1a8225a4e88ced927572ac2440817662ddc2
kaliyappan <yappank17@gmail.com>
2024-08-29 14:14:47 +0530

    tiemsheet fetch and edit and delete option as enabled

--- backend/routes/teamRoutes.js
+++ backend/routes/teamRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Team = require('../models/teamModel.js');
 
+
 // Create a new team member
 router.post('/add', async (req, res) => {
   const { name, role, contact } = req.body;
@@ -24,5 +25,6 @@ router.get('/', async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
+
 
 module.exports = router;
